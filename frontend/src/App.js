2ccc566b77f6bbfddd5138feeaa548e970e867c8
MarkNewcomb1 <wlmmn@hotmail.com>
2020-10-02 12:05:32 -0600

    products overview page rendering

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
 import Footer from './components/Footer';
 const App = () => {
   return (
     <>
-    <Header />
-    <main className='py-3'>
-      <Container>
-    <h1>Welcome to Proshop</h1>
-      </Container>
-    </main>
-    <Footer />
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
